5ea2140d8f1f72690d69933f8d91ae4e4bbba067
tosh404 <maquintosh404@gmail.com>
2022-11-19 15:31:22 +0900

    added testing libraries on /frontend.

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -3,6 +3,7 @@ import { Button } from 'ui';
 function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <h2>Next.js!</h2>
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20  text-center">
         <Button />
         <Button />
